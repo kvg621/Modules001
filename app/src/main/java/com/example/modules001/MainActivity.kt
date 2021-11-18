d5c4d9d5644a1cf6aeedb30b6b69f8ed7758e3e3
kvg621 <konst000@yandex.ru>
2021-11-18 20:25:50 +0400

    Add fragments yo modules

--- app/src/main/java/com/example/modules001/MainActivity.kt
+++ app/src/main/java/com/example/modules001/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.example.modules001
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.module002.BlankFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private var menuBottomBar: BottomNavigationView? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button001 = findViewById<Button>(R.id.button_module001)
-        button001.setOnClickListener() {
-   Toast.makeText(this,"Click Button",Toast.LENGTH_SHORT)
+        val menuFragment = BlankFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fregment001, menuFragment)
+            .commit()
+        val menuBottomBar = findViewById<BottomNavigationView>(R.id.nav_bar_bottom)
+        menuBottomBar.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.to_002 -> {
+                    val bFragment = BlankFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fregment001, bFragment)
+                        .commit()
+                }
+                R.id.to_home -> {
+                    val mFragment = mainFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fregment001, mFragment)
+                        .commit()
+                }
+            }
+            true
         }
+
     }
 }
--- app/src/main/java/com/example/modules001/dependencies.kt
+++
@@ -1,7 +0,0 @@
-package com.example.modules001
-
-object Plugins {
-
-    const val androidApplication    = "com.android.application"
-    const val androidLibrary        = "com.android.library"
-}
